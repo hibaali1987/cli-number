73e8d89206d79d56b465a9561148e99368fe0812
hibaali1987 <hibamaqsood_19@hotmail.com>
2024-03-30 14:35:04 +0500

    created no guessing game

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) computer will generate a random number
+// 2) user input for guessing number
+// 3) compare user input with computer generated number and show result - done
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a numberbetween 1-10: ",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulation! you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number");
+}
